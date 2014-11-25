8855eaeb6378edc748b485074b5f6b755a59babb
Martin Ueding <dev@martin-ueding.de>
2014-11-25 11:08:53 +0100

    Plot residuals

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import scipy.stats
 import loader
 
 def fit_and_plot(func, x, y, yerr, axes, omit_pre=0, omit_post=0, p0=None,
-                 fit_param={}, data_param={}, used_param={}):
+                 fit_param={}, data_param={}, used_param={}, axes_res=None):
 
     if omit_post == 0:
         used_x = x[omit_pre:]
@@ -52,6 +52,11 @@ def fit_and_plot(func, x, y, yerr, axes, omit_pre=0, omit_post=0, p0=None,
     param = {'marker': '+', 'linestyle': 'none'}
     param = dict(param.items() + used_param.items())
     axes.errorbar(used_x, used_y, yerr=used_yerr, **param)
+
+    axes_res = axes.twinx()
+    axes_res.errorbar(used_x, used_y - func(used_x, *popt), yerr=used_yerr,
+                      marker='+', linestyle='none', color='red', alpha=0.3)
+    axes_res.set_ylabel('Residual')
 
     dof = len(used_y) - len(popt) - 1
     chisq, p = scipy.stats.chisquare(used_y, func(used_y, *popt), ddof=len(popt))
@@ -161,7 +166,10 @@ def main():
 
     val, err = loader.average_loader(options.filename)
 
+    print('Correlators:')
     plot_correlator(val, err)
+    print()
+    print('Effective Mass:')
     plot_effective_mass(val, err)
 
 def plot_correlator(val, err):
@@ -174,8 +182,9 @@ def plot_correlator(val, err):
     time_folded = np.array(range(len(folded_val)))
 
     fig = pl.figure()
-    ax = fig.add_subplot(1, 2, 1)
-    ax2 = fig.add_subplot(1, 2, 2)
+    fig_f = pl.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax2 = fig_f.add_subplot(1, 1, 1)
 
     ax.errorbar(time, real_val, yerr=real_err, linestyle='none', marker='+', label='complete')
     ax2.errorbar(time_folded, folded_val, yerr=folded_err, linestyle='none', marker='+', label='folded')
@@ -189,10 +198,10 @@ def plot_correlator(val, err):
                      used_param=used_param, data_param=data_param)
     print('Fit parameters cosh:', p[0])
 
-    p = fit_and_plot(exp_fit, time_folded, folded_val, folded_err, ax2, omit_pre=13,
-                     p0=[0.222, 700, 0], fit_param=fit_param,
+    p = fit_and_plot(cosh_fit, time_folded, folded_val, folded_err, ax2, omit_pre=13,
+                     p0=[0.222, 700, 30, 0], fit_param=fit_param,
                      used_param=used_param, data_param=data_param)
-    print('Fit parameters exp:', p[0])
+    print('Fit parameters folded:', p[0])
 
     ax.set_yscale('log')
     ax.margins(0.05, tight=False)
@@ -209,11 +218,14 @@ def plot_correlator(val, err):
     ax2.set_ylabel(r'$\frac{1}{2} [C(t) + C(T-t)]$')
     #ax2.legend(loc='best')
     ax2.grid(True)
-    ax2.yaxis.tick_right()
-    ax2.yaxis.set_label_position('right')
+    #ax2.yaxis.tick_right()
+    #ax2.yaxis.set_label_position('right')
 
     fig.tight_layout()
-    fig.savefig('folded.pdf')
+    fig.savefig('correlator.pdf')
+
+    fig_f.tight_layout()
+    fig_f.savefig('folded.pdf')
 
 def plot_effective_mass(val, err):
     time = np.arange(len(val))
