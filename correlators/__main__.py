305b4ff6b45e22e74b38066372286edba2f381ab
Martin Ueding <dev@martin-ueding.de>
2015-01-20 17:04:48 +0100

    Add colormapper 9 color

--- correlators/__main__.py
+++ correlators/__main__.py
@@ -25,6 +25,18 @@ import pandas as pd
 import correlators.traversal
 import unitprint
 
+COLORMAPPER_9 = [
+    '#e41a1c',
+    '#377eb8',
+    '#4daf4a',
+    '#984ea3',
+    '#ff7f00',
+    '#ffff33',
+    '#a65628',
+    '#f781bf',
+    '#999999',
+]
+
 
 def main():
     options = _parse_args()
